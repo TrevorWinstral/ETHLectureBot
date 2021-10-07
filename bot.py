4879e8c0ea4597479111f88dbc421972746ff61d
TrevorWinstral <trevor.winstral@gmail.com>
2021-10-07 09:29:41 +0200

    added different markup to test

--- bot.py
+++ bot.py
@@ -225,15 +225,19 @@ for dept in courses.keys():
                 except Exception as e:
                     logger.log(20, f'Something went wrong when trying to send the user {sub} an update for their course {c.name}: {c.code}. Error: {e}')
 
-'''       
+      
 c = courses['d-math'][0]
 c_text = commandify(c.name+'_'+c.prof)
+markup1 = types.InlineKeyboardMarkup()
+markup1.add(types.InlineKeyboardButton('Unsub from this course', callback_data='#UnsubFrom'+c_text))
+logger.log(20, '#UnsubFrom'+c_text)
+markup1.add(types.InlineKeyboardButton('Sub to another course', callback_data='sub'), types.InlineKeyboardButton('Unsub from another course', callback_data='unsub') )
+
 markup = types.InlineKeyboardMarkup()
 markup.add(types.InlineKeyboardButton('Unsub from this course', callback_data='#UnsubFrom'+c_text))
-logger.log(20, '#UnsubFrom'+c_text)
-markup.add(types.InlineKeyboardButton('Sub to another course', callback_data='sub'), types.InlineKeyboardButton('Unsub from another course', callback_data='unsub') )
-bot.send_message(admins[0], f"The course {c.name} has been updated! Check out {c.course_url}\nUse /help for help or to report a problem.", reply_markup=markup)
-'''
+markup.add(types.InlineKeyboardButton('Sub to a course', callback_data='sub'), types.InlineKeyboardButton('Unsub from a course', callback_data='unsub') )
+bot.send_message(admins[0], f"The course {c.name} has been updated! Check out {c.course_url}\nUse /help for help or to report a problem.", reply_markup=markup1)
+
 
 
 with open('rss_courses.pkl', 'wb') as outf:
